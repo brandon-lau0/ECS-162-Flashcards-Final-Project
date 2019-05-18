5980df921ff742dc2530cbbcb8f7d8aed1dcda55
panda <daniel.phan36@gmail.com>
2019-05-17 21:02:44 -0700

    add basic blocks for translation API

---
+++ flashcards_project/public/script.jsx
@@ -0,0 +1,50 @@
+///////////////////////////////////////////////////////////////////////////////
+//                               Add Card Page                               //
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+ * The CSS selector of the English card.
+ */
+const EN_ADD_CARD_SELECTOR = "#card-en-add";
+
+/*
+ * The CSS selector of the translated card.
+ */
+const TL_ADD_CARD_SELECTOR = "#card-tl-add";
+
+/*
+ * Requests the translation of TEXT, then calls CALLBACK with the
+ * translation as the sole parameter.
+ */
+function requestTranslation(text, callback) {
+  let request = new XMLHttpRequest();
+  request.open("GET", `translate?english=${text}`, true);
+
+  // TODO: change Chinese if branny names it something different
+  request.onload = () => callback(JSON.parse(request.responseText).Chinese);
+  request.onerror = () => alert("There was an error with translation request.");
+
+  request.send();
+}
+
+/*
+ * Watches for when the user presses RET and makes the translation
+ * request accordingly.
+ * TODO: this name sucks
+ */
+function addCardKeyDown(event) {
+  console.log(event);
+  if (event.keyCode == 13) {
+    console.log("fuck");
+    const input = document.querySelector(EN_ADD_CARD_SELECTOR).value;
+
+    event.preventDefault();
+    requestTranslation(input, translation => {
+      console.log(`${input}\n${translation}`);
+      // TODO: this will be react actually
+      document.querySelector(TL_ADD_CARD_SELECTOR).innerText = translation;
+    });
+  }
+}
+
+// TODO: database, etc.
