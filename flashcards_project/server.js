2a74cdbe9a5fc4ceb38a11a03b09a6899e5a34aa
Brandon Lau <bylau@ucdavis.edu>
2019-05-18 18:37:55 -0700

    translate API call from server works now

--- flashcards_project/server.js
+++ flashcards_project/server.js
@@ -1,4 +1,12 @@
+"use strict";
+
 const express = require('express')
+const APIrequest = require('request');
+const http = require('http');
+
+const APIkey = "AIzaSyAt9g5D8RZEYrisegFBSwmVazNBn0I3tP0";  // Google Translate API Key
+const url = "https://translation.googleapis.com/language/translate/v2?key=" + APIkey
+
 const port = 3000
 const sqlite3 = require("sqlite3").verbose();  // use sqlite
 const fs = require("fs"); // file system
@@ -37,18 +45,67 @@ function queryHandler(req, res, next) {
     }
 }
 
+function translateTextHandler(req, res, next) {
+  // browser sends request to server in the format: {english : "text"}
+  let qObj = req.query;
+
+  if (qObj != undefined) {
+    let requestObj =
+      {
+        "source": "en",
+        "target": "zh-TW",
+        "q": [
+          qObj.english
+        ]
+      };
+
+    APIrequest(
+      {
+        url: url,
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        json: requestObj
+      },
+      TranslateAPICallback  // translateAPI callback function
+    );
+  } else {
+    next();
+  }
+}
+
+function TranslateAPICallback(err, APIResHead, APIResBody) {
+  if ((err) || (APIResHead.statusCode != 200)) {
+    // API not working
+    console.log("API error, not working");
+    console.log(APIResBody);
+  } else {
+    if (APIResHead.error) {
+      // API request worked but not giving data
+      console.log("API request worked, but no data returned");
+      console.log(APIResHead.error);
+    } else {
+      console.log("In Chinese: ", 
+          APIResBody.data.translations[0].translatedText);
+      console.log("\n\nJSON was:");
+      console.log(JSON.stringify(APIResBody, undefined, 2));
+
+      // NEED TO SEND THE TRANSLATED TEXT BACK TO CLIENT
+    }
+  }
+}
+
 function fileNotFound(req, res) {
     let url = req.url;
     res.type('text/plain');
     res.status(404);
-    // res.send('Cannot find '+url);
-    res.send('Hello there!! '+url);
+    res.send('Cannot find ' + url);
 }
 
 // put together the server pipeline
 const app = express()
 app.use(express.static('public'));  // can I find a static file?
-app.get('/query', queryHandler );   // if not, is it a valid query?
+app.post('/translate', translateTextHandler);
+//app.get('/query', queryHandler );   // if not, is it a valid query?
 app.use( fileNotFound );            // otherwise not found
 
 // app.listen(port, function (){console.log('Listening... Do something now!');} )
