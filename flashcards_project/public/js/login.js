4b66ab4b4b6059f32f1ef887204b36ebc9af7f48
panda <daniel.phan36@gmail.com>
2019-06-03 13:24:00 -0700

    move files

--- flashcards_project/public/js/login.js
+++
@@ -1,7 +0,0 @@
-function login() {
-  let request = new XMLHttpRequest();
-  request.open("GET", "auth/google", true);
-  request.onload = () => console.log("Login success");
-  request.onerror = () => console.log("Login error");
-  request.send();
-}
